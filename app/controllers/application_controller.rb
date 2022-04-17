dfef21b46e9bc11119c4060f4117a6631acf282a
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-04-17 15:42:34 -0400

    commit tree 7fe6986c87bd27c085320def0b95ba974e8b23d1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < Sinatra::Base
-
+ set :default_content_type , 'application/json'
   # add routes
+    get '/bakeries' do
+      bakeries = Bakery.all
+      # send them back as a JSON array
+      bakeries.to_json
+    end
 
 end
